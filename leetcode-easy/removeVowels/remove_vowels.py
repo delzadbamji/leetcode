42da0f110806ba843d45b36d9aefe68a1a902cd4
Delzad Bamji <delzadbamji@gmail.com>
2021-05-24 15:54:18 -0400

    Add files via upload

---
+++ leetcode-easy/removeVowels/remove_vowels.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeVowels(self, s: str) -> str:
+        st=""
+        for i in s:
+            if i not in ("a","e","i","o","u"):
+                st+=i
+        return st
